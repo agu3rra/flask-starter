270b138452c5b06864c56b77f754c209705f1eb2
Andre <agu3rra@gmail.com>
2019-10-19 07:17:23 -0300

    simple use case with Blueprints

--- application/simpleapi/app.py
+++ application/simpleapi/app.py
@@ -1,3 +1,29 @@
-from flaskr import create_app
+import os
+import json
+from flask import Flask, Response
+import routes
 
-application = create_app()
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
+
+# Ensure my instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+# My status response
+@app.route('/')
+def status():
+    response = {
+        "status":True,
+        "info":"I am alive!"
+    }
+    response = json.dumps(response)
+    return Response(response, status=200, mimetype='application/json')
+
+app.register_blueprint(routes.users_resource)
